4cdb7f064a10b49712c00ae7801bbfe24df104a7
arrowe <einhauserfabio@gmail.com>
2023-03-15 21:48:54 +0100

    V0.1.3

---
+++ src/Stats.java
@@ -0,0 +1,45 @@
+public class Stats {
+
+    //Iv werte noch
+    //Ev sclaing
+
+    //Healt stats
+    private double health_raw;
+    private double health;
+    private double health_current;
+
+    //Attack stats
+    private double attack_raw;
+    private double attack;
+    private double attack_current;
+
+    //Defense stats
+    private double defense_raw;
+    private double defense;
+    private double defense_current;
+
+    //sp.Atk stats
+    private double special_attack_raw;
+    private double special_attack;
+    private double special_attack_current;
+
+    //sp.Def stats
+    private double special_defense_raw;
+    private double special_defense;
+    private double special_defense_current;
+
+    //speed stats
+    private double speed_raw;
+    private double speed;
+    private double speed_current;
+
+    //Elemente zuweisung
+    private double element_fire = 1;
+    private double element_water = 2;
+    private double element_grass = 3;
+    private double element_flight = 4;
+    private double element_bunny;
+
+    //multiplyer stat
+    public double final_multiplyer = 1.1;
+}
