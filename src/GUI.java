1d5044e5be0acedff025941b43984072c5979ec6
kennatris <tino.st.2004@gmail.com>
2023-03-15 21:19:04 +0100

    V0.1

---
+++ src/GUI.java
@@ -0,0 +1,30 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class GUI {
+    public boolean fullscreen;
+    private int width;
+    private int height;
+
+    public GUI() {
+        this.fullscreen = true;
+        this.width = 1152;
+        this.height = 576;
+    }
+
+    public void openWindow() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Fabimon");
+        frame.getContentPane().setBackground(new Color(34, 139, 34)); // set background color to green
+
+        if (fullscreen) {
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // maximize the window
+        } else {
+            frame.setSize(width, height); // set window size to height x width
+            frame.setLocationRelativeTo(null); // center the window
+        }
+
+        frame.setVisible(true);
+    }
+}
---
+++ src/KeyHandler.java
@@ -0,0 +1,82 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    public boolean wPressed, aPressed, dPressed, sPressed, escPressed, spacePressed, iPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {    // Wenn eine Taste gedrückt wird wird diese Methode aufgerufen.
+
+        int code = e.getKeyCode();  // Speichert den Code der Taste die gedrückt wird in einer Variable ab.
+
+        if (code == KeyEvent.VK_W) {    // schaut nach welche Taste gedrückt wurde und setzt den jeweiligen Boolean auf true.
+            wPressed = true;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            aPressed = true;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            dPressed = true;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            sPressed = true;
+        }
+
+        if (code == KeyEvent.VK_SPACE) {
+            spacePressed = true;
+        }
+
+        if (code == KeyEvent.VK_ESCAPE) {
+            escPressed = true;
+        }
+
+        if (code == KeyEvent.VK_I) {
+            iPressed = true;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {   //Wenn die Taste losgelassen wurde wird der jeweilige Boolean auf false gesetzt.
+
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            wPressed = false;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            aPressed = false;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            dPressed = false;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            sPressed = false;
+        }
+
+        if (code == KeyEvent.VK_SPACE) {
+            spacePressed = false;
+        }
+
+        if (code == KeyEvent.VK_ESCAPE) {
+            escPressed = false;
+        }
+
+        if (code == KeyEvent.VK_I) {
+            iPressed = false;
+        }
+
+    }
+}
