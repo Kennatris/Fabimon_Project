815b07096d1b2334f0d8b7489605413606dd321a
Philipp <philipp.kamerlocher@gmail.com>
2023-03-17 16:26:54 +0100

    V0.1.3.8

--- src/main/GameHandler.java
+++ src/main/GameHandler.java
@@ -1,30 +1,24 @@
 package main;
 
-import objects.Player;
-
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.security.Key;
-
 public class GameHandler {
+    public long timer = 0;
     // var_delta-timer
     int FPS = 60;
-    public long timer = 0;
     // Integration
     KeyHandler keyH = new KeyHandler();
 
     // start the code
     public void start_setup() {
-            // open a new Instance of GUI
-            GUI myGUI = new GUI(true, 1152, 576);
-            myGUI.openWindow();
+        // open a new Instance of GUI
+        GUI myGUI = new GUI(true, 1152, 576);
+        myGUI.openWindow();
 
-            // starts the internal Delta-Timer ()
-            delta_timer(myGUI);
+        // starts the internal Delta-Timer ()
+        delta_timer(myGUI);
     }
 
     public void delta_timer(GUI myGUI) {
-        double drawInterval = 1000000000/FPS; // 0.01666666 seconds
+        double drawInterval = 1000000000 / FPS; // 0.01666666 seconds
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
@@ -49,6 +43,7 @@ public class GameHandler {
     }
 
     public void update() {
+
 
     }
 }
--- src/main/Kontroller.java
+++ src/main/Kontroller.java
@@ -3,36 +3,82 @@ package main;
 import net.java.games.input.*;
 
 public class Kontroller {
-    public void controller() {
+
+
+
+    Boolean test;
+
+    public void connectcontroller() {
+
+
         Controller[] controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
         Controller controller = null;
 
-        for (int i = 0; i < controllers.length; i++) {
-            if (controllers[i].getType() == Controller.Type.GAMEPAD)
-                controller = controllers[i];
+        for (Controller value : controllers) {
+            if (value.getType() == Controller.Type.GAMEPAD)
+                controller = value;
         }
 
         EventQueue eventQueue = controller.getEventQueue();
         Event event = new Event();
         Boolean stopped = false;
+        test = true;
 
         while (!stopped) {
             controller.poll();
-            eventQueue.getNextEvent(event);
+            test = controller.poll();
 
-            Component component = event.getComponent();
+            if (!test) {
+                System.out.println("test");
 
-            if (component != null) {
-                Component.Identifier identifier = component.getIdentifier();
-                float data = component.getPollData();
-                if (identifier == Component.Identifier.Button._0) {
-                    System.out.println(data);
-                } else if (identifier == Component.Identifier.Axis.RZ) {
-                    System.out.println(data);
+                DirectAndRawInputEnvironmentPlugin directEnv = new DirectAndRawInputEnvironmentPlugin();
+                if (directEnv.isSupported()) {
+                    controllers = directEnv.getControllers();
+                } else {
+                    controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+                }
+
+                for (Controller value : controllers) {
+                    if (value.getType() == Controller.Type.GAMEPAD)
+                        controller = value;
+                }
+
+                eventQueue = controller.getEventQueue();
+                event = new Event();
+            } else {
+
+
+                eventQueue.getNextEvent(event);
+
+                Component component = event.getComponent();
+
+                if (component != null) {
+                    Component.Identifier identifier = component.getIdentifier();
+                    float data = component.getPollData();
+                    if (identifier == Component.Identifier.Button._0) {
+                        System.out.println(data);
+                    } else if (identifier == Component.Identifier.Axis.RZ) {
+                        if (data > 0.2 || data < -0.2) {
+                            System.out.println(data);
+                        }
+
+                    } else if (identifier == Component.Identifier.Axis.Z) {
+                        if (data > 0.2 || data < -0.2) {
+                            System.out.println(data);
+                        }
+                    } else if (identifier == Component.Identifier.Axis.X) {
+                        if (data > 0.2 || data < -0.2) {
+                            System.out.println(data);
+                        }
+                    } else if (identifier == Component.Identifier.Axis.Y) {
+                        if (data > 0.2 || data < -0.2) {
+                            System.out.println(data);
+                        }
+                    }
+
                 }
             }
-
-
         }
+
     }
 }
