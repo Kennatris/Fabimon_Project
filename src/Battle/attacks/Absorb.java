db65237ff646daeb81cb325fea1e87eb4d7884f4
FabioEinhauser <79258048+FabioEinhauser@users.noreply.github.com>
2023-05-24 08:58:21 +0200

    Add files via upload

---
+++ src/Battle/attacks/Absorb.java
@@ -0,0 +1,24 @@
+package Battle.attacks;
+
+import Battle.Attack;
+import main.GameHandler;
+
+public class Absorb extends Attack {
+    GameHandler gameH;
+
+    public Absorb(GameHandler gameH) {
+        super(gameH);
+        this.gameH = gameH;
+        getButtonImage("Absorb");
+        category = "special";
+        type = "Grass";
+        power = 20;
+        accuracy = 100;
+        ap = 25;
+        priority = 0;
+        name = "Absorb";
+        trifftImmer = false;
+        target = "enemy";
+
+    }
+}
---
+++ src/Battle/attacks/Bubble.java
@@ -0,0 +1,24 @@
+package Battle.attacks;
+
+import Battle.Attack;
+import main.GameHandler;
+
+public class Bubble extends Attack {
+    GameHandler gameH;
+
+    public Bubble(GameHandler gameH) {
+        super(gameH);
+        this.gameH = gameH;
+        getButtonImage("Bubble");
+        category = "special";
+        type = "Water";
+        power = 40;
+        accuracy = 100;
+        ap = 30;
+        priority = 0;
+        name = "Bubble";
+        trifftImmer = false;
+        target = "enemy";
+
+    }
+}
